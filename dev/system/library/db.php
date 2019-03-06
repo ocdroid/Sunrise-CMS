7820c6a16b9291b82b8b9ae85d3d6d22692f9233
Nick Burakov <burakov.work@gmail.com>
2019-03-06 11:33:11 +0200

    formating fixes

--- dev/system/library/db.php
+++ dev/system/library/db.php
@@ -1,39 +1,44 @@
 <?php
-
-
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
-class DB {
-	private $adaptor;
+class DB
+{
+    private $adaptor;
 
-	public function __construct($adaptor, $hostname, $username, $password, $database, $port = NULL) {
-		$class = 'DB\\' . $adaptor;
+    public function __construct($adaptor, $hostname, $username, $password, $database, $port = null)
+    {
+        $class = 'DB\\' . $adaptor;
 
-		if (class_exists($class)) {
-			$this->adaptor = new $class($hostname, $username, $password, $database, $port);
-		} else {
-			throw new \Exception('Error: Could not load database adaptor ' . $adaptor . '!');
-		}
-	}
+        if (class_exists($class)) {
+            $this->adaptor = new $class($hostname, $username, $password, $database, $port);
+        } else {
+            throw new \Exception('Error: Could not load database adaptor ' . $adaptor . '!');
+        }
+    }
 
-	public function query($sql, $params = array()) {
-		return $this->adaptor->query($sql, $params);
-	}
+    public function query($sql, $params = array())
+    {
+        return $this->adaptor->query($sql, $params);
+    }
 
-	public function escape($value) {
-		return $this->adaptor->escape($value);
-	}
+    public function escape($value)
+    {
+        return $this->adaptor->escape($value);
+    }
 
-	public function countAffected() {
-		return $this->adaptor->countAffected();
-	}
+    public function countAffected()
+    {
+        return $this->adaptor->countAffected();
+    }
 
-	public function getLastId() {
-		return $this->adaptor->getLastId();
-	}
-	
-	public function connected() {
-		return $this->adaptor->connected();
-	}
+    public function getLastId()
+    {
+        return $this->adaptor->getLastId();
+    }
+    
+    public function connected()
+    {
+        return $this->adaptor->connected();
+    }
 }
